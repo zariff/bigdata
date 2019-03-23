9924f1eeb533b1993cbf68b3d3b0b25d05dd1718
Max <maxraadde@hotmail.com>
2019-03-23 15:51:52 +0100

    Added wordlist and dataframe generator

--- data_generating/generate_data.py
+++ data_generating/generate_data.py
@@ -2,46 +2,81 @@
 import matplotlib.pyplot as plt
 import random
 import numpy as np
+import pandas as pd
+import datetime
+from datetime import timedelta
+import csv
 
-sample_size = 5000
-random_walk_weight = 10
-random_walk_variance = 10
-trend = 0.2
-exponent = 1.2
+start_value = 5
+start_time_shift =20
+random_walk_weight = 0.01
+random_walk_variance = 1
+seasonality_1_freq = 50 #lower is shorter
+seasonality_1_weight = 5
+seasonality_2_freq = 7 #lower is shorter
+seasonality_2_weight = 2
+
+sample_size = 500
+
+trend = 0.02
+exponent = 1
+codes = list('abcdefghijklmnopqrstuvwxyz')
+n_samples = 10
+
+terms = []
+with open('wordlist.csv', 'r') as f:
+	reader = csv.reader(f)
+	terms = list(reader)
 
 
-# Get index for time series
-
+# Generates time series
 def generate_ts():
-	index = np.arange(sample_size)
 	sample = []
 	random_walk = np.random.normal(0,random_walk_variance)
+	y = start_value
 
-	for time in index:
+	for time in np.arange(sample_size):
+
+		#Assume no start at y = 0
+		time = time + start_time_shift
 
 		#Add seasonality 1
-		y = np.sin(time/100)*200
+		y = (np.sin(time/seasonality_1_freq))*seasonality_1_weight
+		
 
 		#Add seasonality 2
-		y = y + np.sin(time/250)*400
+		y = y + np.sin(time/seasonality_2_freq)*seasonality_2_weight
 
 		#Add trend
-		y = y + (trend * time ** exponent)
+		y = y + (trend * (time ** exponent))
 
 		# Random walk component
 		random_walk = random_walk + np.random.normal(0,random_walk_variance)
 		y = y + random_walk * random_walk_weight
 		
 
-		sample.append(y)
+		sample.append(max(0, y))
 	return sample
 
+#Plot some time series
 def plot_ts(n):
 	for _ in range(n):
 		plt.plot(generate_ts())
 
+#Creates a dummy dataset from n time series for 'codes'
+def create_dummy_dataset():
+	df = pd.DataFrame({'date': [], 'code': [], 'text': []})
+	for i in range(n_samples):
+		sample = generate_ts()
+		for index, number in enumerate(sample): #Loop over a sample
+			for n in range(int(round(number))): #Create n entries
+				date = datetime.datetime(2000, 1, 1, 0, 0) + timedelta(days=index)
+				df = df.append({'date': date, 'code': codes[i], 'text': random.sample(terms, 10)}, ignore_index=True)
+	return df
 
 
-plot_ts(10)
+dummy_dataset = create_dummy_dataset()
+print(dummy_dataset)
 
-plt.show()
+# plot_ts(10)
+# plt.show()
